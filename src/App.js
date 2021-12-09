b079812b1810d2fffa84d607e839887abb79c1e1
wiktorkulas51 <wiktorkulas51@gmail.com>
2021-12-09 23:24:17 +0100

    no time

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import "./App.css";
 import Box from "./box";
+import { useState } from "react";
 
 function App() {
   const [state, setstate] = useState("cross");
   const toggleOnclick = () => {
     if (state === "cross") {
       return setstate("circle");
-    } else {
+    } else if (state === "circle") {
       return setstate("cross");
     }
   };
+
   return (
     <div className="App">
-      <button onClick={toggleOnclick()}></button>
-      <Box className="box 1"></Box>
-      <Box className="box 2"></Box>
-      <Box className="box 3"></Box>
-      <Box className="box 4"></Box>
-      <Box className="box 5"></Box>
-      <Box className="box 6"></Box>
-      <Box className="box 7"></Box>
-      <Box className="box 8"></Box>
-      <Box className="box 9"></Box>
+      <button
+        className={`btn ${state === "circle" ? "circle" : "cross"}`}
+        onClick={toggleOnclick}
+      ></button>
+      <Box className="box 1" state={state}></Box>
+      <Box className="box 2" state={state}></Box>
+      <Box className="box 3" state={state}></Box>
+      <Box className="box 4" state={state}></Box>
+      <Box className="box 5" state={state}></Box>
+      <Box className="box 6" state={state}></Box>
+      <Box className="box 7" state={state}></Box>
+      <Box className="box 8" state={state}></Box>
+      <Box className="box 9" state={state}></Box>
     </div>
   );
 }
--- src/box.js
+++ src/box.js
@@ -1,23 +1,26 @@
 import React from "react";
 let currentBox;
 
-const giveClassOnclick = (e) => {
-  console.log(e.target.className);
-  currentBox = e.target.className;
-  if (
-    currentBox === `${currentBox} cross` ||
-    currentBox === `${currentBox} circle`
-  ) {
-    alert("pole jest juz zajete!");
-  } else {
-    currentBox += " cricle";
-    console.log(currentBox);
-  }
-};
-
 const Box = (props) => {
+  const giveClassOnclick = (e) => {
+    console.log(e.target.className);
+    currentBox = e.target.className;
+    if (
+      currentBox === `${currentBox} cross` ||
+      currentBox === `${currentBox} circle`
+    ) {
+      alert("pole jest juz zajete!");
+    } else {
+      currentBox += " cricle";
+      console.log(currentBox);
+    }
+  };
+  const { className, state } = props;
+  console.log(state);
   return (
-    <div className={props.className} onClick={(e) => giveClassOnclick(e)}></div>
+    <div className={className} onClick={(e) => giveClassOnclick(e)}>
+      {state === "cricle" ? <div>asd</div> : <div>abc</div>}
+    </div>
   );
 };
 
